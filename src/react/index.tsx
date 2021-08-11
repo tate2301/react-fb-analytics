3475bc5c277eb942a79628dbfdfcbc5e995795b0
Tatenda Christopher Chinyamakobvu <chris@devtachom.com>
2021-08-11 19:46:13 +0200

    fix: add window check

--- src/react/index.tsx
+++ src/react/index.tsx
@@ -6,13 +6,16 @@ export const FacebookEventTrackerContext =
     createContext<FacebookConversion | null>(null)
 
 export const FacebookEventProvider = ({ children, config }: ReactContextProps) => {
-    const fbTracker: FacebookConversion = (
-        new FacebookConversion(
-            config.pixelId,
-            config.testCode,
+    let fbTracker = null
+    if (typeof (window) !== "undefined") {
+        const fbTracker: FacebookConversion = (
+            new FacebookConversion(
+                config.pixelId,
+                config.testCode,
+            )
         )
-    )
-    fbTracker.config(config.apiKey, config.wrapperUrl)
+        fbTracker.config(config.apiKey, config.wrapperUrl)
+    }
 
     return (
         <FacebookEventTrackerContext.Provider value={fbTracker}>
